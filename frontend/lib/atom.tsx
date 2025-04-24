bef1dc2e285fc7b457f8f7524e828b9d66d3dc77
Comet0124 <comet040124@gmail.com>
2025-04-24 06:19:05 -0400

    refactor: reorganize atom declarations in atom.tsx for improved clarity and maintainability

--- frontend/lib/atom.tsx
+++ frontend/lib/atom.tsx
@@ -1,11 +1,6 @@
 import { atom } from "jotai";
 import { Agent } from "@/lib/props";
 
-export const isActiveAtom = atom(false);
-export const agentsAtom = atom<Agent[]>([]);
-export const selectedAgentIdAtom = atom<number>(null);
-export const selectedAgentAtom = atom<Agent>(null);
-
 export const invoiceTableAllColumns = [
   { key: "name", label: "Name" },
   { key: "mobile_number", label: "Mobile Number" },
@@ -19,5 +14,8 @@ export const invoiceTableAllColumns = [
   { key: "actions", label: "Actions" },
 ];
 
+export const isActiveAtom = atom(false);
+export const agentsAtom = atom<Agent[]>([]);
+export const selectedAgentIdAtom = atom<number>(null);
+export const selectedAgentAtom = atom<Agent>(null);
 export const invoiceTableSelectedColumnsAtom = atom<string[]>(invoiceTableAllColumns.map(c => c.key));
-
